0896fdf61f945e0a229dadccc1d0576782140951
AnnnaD <anna.dzich@gmail.com>
2018-06-25 17:10:01 +0200

    changes in aria structure

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -102,7 +102,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
  */
 fillReviewsHTML = (reviews = self.restaurant.reviews) => {
   const container = document.getElementById('reviews-container');
-  const title = document.createElement('h2');
+  const title = document.createElement('h3');
   title.innerHTML = 'Reviews';
   container.appendChild(title);
 
@@ -154,7 +154,9 @@ createReviewHTML = (review) => {
  */
 fillBreadcrumb = (restaurant=self.restaurant) => {
   const breadcrumb = document.getElementById('breadcrumb');
+  breadcrumb.setAttribute('role', 'navigation');
   breadcrumb.setAttribute('aria-label', 'Breadcrumb');
+
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
